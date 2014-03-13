46b55b259a8a5e23bb263c3dca991ba13a397f72
marco9333 <marco.magliona@gmail.com>
2014-03-13 17:52:03 +0100

    n80xx-common: Clean directory, added lpm mode
    Fixed lpm mode charger script building (it should build as it should without touching the files in other trees).
    
    Copied charger source files from android/core to make device specific changes. DONE (Look the sources)
    
    Screen can be switched on after it's blanked and animation will restart.
    (Now it's working, I've decided to use brightness flags for recovery)
    
    Enabled usb charging in lpm too (Needs a mod in the kernel to allow enough current to pass through https://gerrit.omnirom.org/#/c/6286/)
    
    If you disconnect the charger you have to manually shut the tablet off ->TODO
    
    Cleaned recovery_keys file from buttons we don't have
    
    Change-Id: I52aa856ac4c3a357cc91b8e87fbeac4087330e68

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,6 +52,17 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 #           both init scripts can be found in the recovery folder
 # TARGET_RECOVERY_INITRC := device/samsung/n80xx-common/recovery/init.rc
 
+#Charging mode
+BOARD_BATTERY_DEVICE_NAME := "battery"
+BOARD_AC_DEVICE_NAME := "ac"
+BOARD_USB_DEVICE_NAME := "usb"
+BOARD_CHARGER_ENABLE_SUSPEND := false
+BOARD_CHARGER_DIM_SCREEN_BRIGHTNESS := true
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../../device/samsung/n80xx-common/recovery/recovery_keys.c
+#Use default graphics.c overriding the variable in BoardCommonConfig of smdk4412-common
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/n80xx-common/recovery/graphics.c
+
+
 # TWRP specific build flags
 DEVICE_RESOLUTION := 1280x800
 TW_INTERNAL_STORAGE_PATH := "/data/media"
